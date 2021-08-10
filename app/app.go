06ec94b918140a84bba1193041aa7a1cf9dfd5b0
niharika-tw <nraheja1995@gmail.com>
2021-08-10 12:23:07 +0530

    Partha,Niharika | Allow only Post method for our booking service

--- app/app.go
+++ app/app.go
@@ -1,31 +1,24 @@
 package app
 
 import (
-	"fmt"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func StartServer() {
-	http.HandleFunc("/book", bookHandler())
-	http.HandleFunc("/", pingHandler())
 
-	if err := http.ListenAndServe(":8080", nil); err != nil && err != http.ErrServerClosed {
-		fmt.Errorf("something went wrong %s", err)
-		fmt.Println("Server Not Started")
-		return
-	}
-	fmt.Println("Server Started")
-}
+	r := gin.Default()
+	r.POST("/book", func(c *gin.Context) {
+		
+		c.JSON(201, gin.H{
+			"message": "Ticket Created",
+		})
+	})
 
-func pingHandler() http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		writer.WriteHeader(http.StatusOK)
-	}
-}
-
-func bookHandler() http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println("Ticket Created")
-		writer.WriteHeader(http.StatusCreated)
-	}
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Server started",
+		})
+	})
+	go r.Run()
+	go r.Run()
 }
